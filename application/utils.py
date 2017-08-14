7fae6af1c2ff5e978428eea35d211a6524165794
jneubrand <johannes_n@icloud.com>
2017-08-14 09:54:36 -0700

    Add missing utils file

---
+++ application/utils.py
@@ -0,0 +1,7 @@
+def get_price(results, symbol):
+    stock_obj = [results[x] for x in range(len(results))
+                 if (type(results[x]) is not None and
+                 results[x]['symbol'] == symbol)][0]
+    return (stock_obj['last_extended_hours_trade_price']
+            if stock_obj['last_extended_hours_trade_price'] is not None
+            else stock_obj['last_trade_price'])
