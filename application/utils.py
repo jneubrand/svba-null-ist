05ed96be9a3c581895b7079c0ee511a9285aa13a
jneubrand <johannes_n@icloud.com>
2017-09-14 09:58:30 -0700

    fix util handling of missing fields

--- application/utils.py
+++ application/utils.py
@@ -1,7 +1,11 @@
 def get_price(results, symbol):
-    stock_obj = [results[x] for x in range(len(results))
-                 if (type(results[x]) is not None and
-                 results[x]['symbol'] == symbol)][0]
+    stock_objs = [results[x] for x in range(len(results))
+                  if results[x] is not None and
+                  results[x]['symbol'] == symbol]
+    try:
+        stock_obj = stock_objs[0]
+    except Exception as e:
+        return None
     return (stock_obj['last_extended_hours_trade_price']
             if stock_obj['last_extended_hours_trade_price'] is not None
             else stock_obj['last_trade_price']
