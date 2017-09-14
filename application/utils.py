cf3cbb224aeb63c44ca6090ced436d6d87200713
jneubrand <johannes_n@icloud.com>
2017-09-14 09:21:41 -0700

    fallback for missing last trade price

--- application/utils.py
+++ application/utils.py
@@ -4,4 +4,6 @@ def get_price(results, symbol):
                  results[x]['symbol'] == symbol)][0]
     return (stock_obj['last_extended_hours_trade_price']
             if stock_obj['last_extended_hours_trade_price'] is not None
-            else stock_obj['last_trade_price'])
+            else stock_obj['last_trade_price']
+            if stock_obj['last_trade_price'] is not None
+            else (stock_obj['ask_price'] + stock_obj['bid_price']) / 2)
